41f121e7b349ba4fa158e364e48c0d70584f192d
marcetin <marcetin@gmail.com>
2021-10-02 16:46:19 +0200

    admin

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 11121),
+  url: 'https://api.parallelcoin.io',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a1f691d9c4a55c7fb6fa9dbb798f16b4'),
