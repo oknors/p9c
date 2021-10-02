dd41c26ee44417a25fb6e9831a555f0b34b32d24
marcetin <marcetin@gmail.com>
2021-10-02 17:04:29 +0200

    proxy

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 11121),
   url: 'https://api.parallelcoin.io',
+  proxy:true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a1f691d9c4a55c7fb6fa9dbb798f16b4'),
