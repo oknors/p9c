3204ac785c2322d74d430cd81bf96bc0eea7fe9b
marcetin <marcetin@gmail.com>
2021-10-13 21:29:19 +0200

    da

--- svelte/src/lib/sub/Where.svelte
+++ svelte/src/lib/sub/Where.svelte
@@ -1,28 +1,54 @@
 <script>
       import HeadTitle from '$lib/components/HeadTitle.svelte';
       import Socials from '$lib/components/Socials.svelte';
+
+   
+
+       
+
+
+    
+	
+    import { onMount } from "svelte";
+
+  const apiURL = "https://api.parallelcoin.info/subs";
+  let data = [];
+
+
+  onMount(async function() {
+        const response = await fetch(apiURL);
+        data = await response.json();
+    });
+
+
 </script>
-<div class="flex flex-col text-center parallelcoin">rrrrr
+
+
+
+
+<div class="flex flex-col text-center parallelcoin">
+
+
+
+    {#each data as item }
+            <div>
+                <p> {item.title} </p>
+            </div>
+        {/each}
+
+
         <HeadTitle>Where is ParallelCoin???</HeadTitle>
         <div class="my-24 worp" >
-sssss
-            <amp-list layout="responsive" width="600" height="300" src="https://api.parallelcoin.info/subs" reset-on-refresh single-item
-                  binding="no">
-                  {`{{ . }}`}
-            <template type="amp-mustache">
-  <a class="flex flex-row justify-center my-4 text-2xl no-underline text-text-primary" href="{`'https://'{{ slug }}'.parallelcoin.info'`}">
+            
+  <!-- <a class="flex flex-row justify-center my-4 text-2xl no-underline text-text-primary" href="{`'https://'{{ slug }}'.parallelcoin.info'`}">
             <div class="character">{`{{ title }}`}</div>
             <div class="flex-grow hidden mx-1 mb-2 border-b-2 border-dotted md:flex"></div>
             <div class="hidden md:flex">{`{{ slug }}`}.parallelcoin.info</div>
           </a>
-        </template>
-        </amp-list>
-
-
+         -->
 
         
         </div>
-        <p class="worp"><Socials></Socials></p>
              <amp-img
                 alt="ParallelCoin - Plan 9 from Crypto-Space - Ruler"
                 src="https://s.okno.rs/parallelcoin/img/rulerdesk.png"
@@ -33,3 +59,7 @@ sssss
         >
         </amp-img>
       </div>
+
+
+
+      
