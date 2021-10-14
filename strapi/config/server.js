332c36f5126717ac021e4ff47339a7279f313138
marcetin <marcetin@gmail.com>
2021-10-14 10:07:22 +0200

    da

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 11122),
-  url: 'http://localhost',
+  url: 'http://127.0.0.1',
   proxy:true,
   admin: {
     auth: {
